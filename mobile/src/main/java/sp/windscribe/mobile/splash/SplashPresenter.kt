aaaf2bcaa2d01b3f82533c9068e8d90278136457
MehraB832 <mehrabshafae@gmail.com>
2024-05-10 08:39:21 +0330

    add load servers in splash

--- mobile/src/main/java/sp/windscribe/mobile/splash/SplashPresenter.kt
+++ mobile/src/main/java/sp/windscribe/mobile/splash/SplashPresenter.kt
@@ -1,6 +1,9 @@
 package sp.windscribe.mobile.splash
 
+import sp.windscribe.mobile.GetServersQuery
+
 interface SplashPresenter {
     fun checkNewMigration()
     fun onDestroy()
+    fun setDataAndLoad(data: GetServersQuery.Data?)
 }
