17606566d0dda0c4622493c01570a0baabe1a1fd
MehraB832 <mehrabshafae@gmail.com>
2024-05-10 08:51:17 +0330

    ..

--- mobile/src/main/java/sp/windscribe/mobile/splash/SplashActivity.kt
+++ mobile/src/main/java/sp/windscribe/mobile/splash/SplashActivity.kt
@@ -6,6 +6,7 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.apollographql.apollo3.api.Error
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -26,20 +27,20 @@ import sp.windscribe.vpn.qq.MmkvManager
 import javax.inject.Inject
 
 @SuppressLint("CustomSplashScreen")
-class SplashActivity : BaseActivity(), SplashView {
+class SplashActivity : AppCompatActivity() {
     private val logger = LoggerFactory.getLogger("splash_a")
 
-    @Inject
-    lateinit var presenter: SplashPresenter
+//    @Inject
+//    lateinit var presenter: SplashPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        DaggerActivityComponent.builder().activityModule(ActivityModule(this, this))
-            .applicationComponent(
-                appContext
-                    .applicationComponent
-            ).build().inject(this)
+//        DaggerActivityComponent.builder().activityModule(ActivityModule(this, this))
+//            .applicationComponent(
+//                appContext
+//                    .applicationComponent
+//            ).build().inject(this)
         if (Build.VERSION.SDK_INT >= 23) {
             splashScreen.setKeepOnScreenCondition { true }
         } else {
@@ -48,11 +49,12 @@ class SplashActivity : BaseActivity(), SplashView {
 
         logger.info("OnCreate: Splash Activity")
 
-        if(MmkvManager.getLoginStorage().decodeBool("is_login", false)){
-            this.setup()
-        }else{
-            this.navigateToLogin()
-        }
+//        if(MmkvManager.getLoginStorage().decodeBool("is_login", false)){
+//            this.setup()
+//        }else{
+//            this.navigateToLogin()
+//        }
+        this.navigateToHome()
 
 //        presenter.checkNewMigration()
     }
@@ -68,7 +70,7 @@ class SplashActivity : BaseActivity(), SplashView {
                     object : GetServersWithKeyQuery.GetServersCallback {
                         override fun onSuccess(data: GetServersQuery.Data?) {
                             try{
-                                presenter.setDataAndLoad(data)
+//                                presenter.setDataAndLoad(data)
                             }catch (e:Exception){
                                 Log.d("MRT", "ERR catch2: " + e.toString())
                                 failGetServers()
@@ -97,15 +99,15 @@ class SplashActivity : BaseActivity(), SplashView {
     }
 
     override fun onDestroy() {
-        presenter.onDestroy()
+//        presenter.onDestroy()
         super.onDestroy()
     }
 
-    override fun navigateToAccountSetUp() {
+     fun navigateToAccountSetUp() {
         TODO("Not yet implemented")
     }
 
-    override fun navigateToHome() {
+     fun navigateToHome() {
         logger.info("Navigating to home activity...")
         val homeIntent = Intent(this, WindscribeActivity::class.java)
         if (intent.extras != null) {
@@ -116,7 +118,7 @@ class SplashActivity : BaseActivity(), SplashView {
         finish()
     }
 
-    override fun navigateToLogin() {
+     fun navigateToLogin() {
         logger.info("Navigating to login activity...")
         val loginIntent = Intent(this, WelcomeActivity::class.java)
         startActivity(loginIntent)
