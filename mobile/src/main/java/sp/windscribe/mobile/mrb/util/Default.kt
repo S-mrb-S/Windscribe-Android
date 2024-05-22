52579d2eec062f1a113e5da726855018c367a56a
MehraB832 <mehrabshafae@gmail.com>
2024-05-22 21:49:51 +0330

    clean up

--- mobile/src/main/java/sp/windscribe/mobile/mrb/util/Default.kt
+++
@@ -1,5 +0,0 @@
-package sp.windscribe.mobile.mrb.util
-
-object Default {
-
-}
---
+++ mobile/src/main/java/sp/windscribe/mobile/mrb/util/api/SaveServers.kt
@@ -0,0 +1,35 @@
+package sp.windscribe.mobile.mrb.util.api
+
+import kotlinx.coroutines.coroutineScope
+import sp.windscribe.mobile.GetServersQuery
+import sp.windscribe.mobile.mrb.util.StaticData
+import sp.windscribe.mobile.mrb.util.list.ListCreator
+import sp.windscribe.vpn.qq.Data
+
+suspend fun saveDataAndFinish(data: GetServersQuery.Data?, navigateTo: () -> Unit, failTo: () -> Unit) = coroutineScope {
+    try {
+        try {
+            if (data == null) { // data must be not null
+                Data.dataString = ""
+                failTo()
+                return@coroutineScope
+            }
+            if (StaticData.data == null) {
+                StaticData.data = data // cache for next time
+            }
+
+            Data.dataString = ListCreator(data).createAndGet() // create and save
+        } finally {
+            navigateTo()
+        }
+    } catch (e: Exception) {
+        failTo()
+    }
+}
+
+//fun longLog(str: String) {
+//    if (str.length > 4000) {
+//        Log.d("servers mrb", str.substring(0, 4000))
+//        longLog(str.substring(4000))
+//    } else Log.d("", str)
+//}
