cbc9c92485ff4440a44493d9c2f186a437acfd16
MehraB832 <mehrabshafae@gmail.com>
2024-05-23 14:42:13 +0330

    add complete getter

--- mobile/src/main/java/sp/windscribe/mobile/sp/util/ServiceBackgroundCoroutine.kt
+++ mobile/src/main/java/sp/windscribe/mobile/sp/util/ServiceBackgroundCoroutine.kt
@@ -1,84 +1,46 @@
-//package sp.windscribe.mobile.sp.util
-//
-//import de.blinkt.openvpn.core.OpenVPNThread
-//import dev.dev7.lib.v2ray.V2rayController
-//import kotlinx.coroutines.*
-//import sp.openconnect.core.OpenVpnService
-//import sp.openconnect.core.VPNConnector
-//import sp.windscribe.mobile.sp.util.api.updateService
-//import sp.windscribe.vpn.sp.Data
-//
-// runtime update
-//class ServiceBackgroundCoroutine {
-//
-//    private var job: Job? = null
-//    private var stopService: Boolean = false
-//
-//    fun start() {
-//        job = CoroutineScope(Dispatchers.Default).launch {
-//            val licence = Data.serviceStorage.decodeString("key_login", "")
-//            if(licence.isNullOrEmpty()) {
-//                stop()
-//                return@launch
-//            }
-//
-//                Data.static.showToast("سرویس شروع شد")
-//
-//
-//            while (isActive) {
-//                delay(5000)
-//
-//                updateService(licence.toString(), {},
-//                        {
-//
-//                                Data.static.showToast("اجرای سرویس موفقیت امیز نبود!")
-//                            Data.static.showToast("سرویس شما تا 15 ثانیه دیگر قطع میشود!")
-//
-//                                stopService = true
-//
-//                        })
-//
-//                if(stopService){
-//                    // ۱۵ ثانیه تاخیر
-//                    delay(15000)
-//                    try {
-//                        V2rayController.stopV2ray(Data.static.mainApplication) // v2ray
-//                        OpenVPNThread.stop() // openvpn
-//
-//                        // cisco
-//                        val cc = object : VPNConnector(Data.static.mainApplication, false) {
-//                            override fun onUpdate(service: OpenVpnService) {}
-//                        }
-//                        // IDN
-//                        // Hard stop
-//                        try{
-//                            cc.stop()
-//                        }catch (ignore: Exception){}
-//                        try{
-//                            cc.unbind()
-//                        }catch (ignore: Exception){}
-//                        try{
-//                            cc.stopActiveDialog()
-//                        }catch (ignore: Exception){}
-//                        try{
-//                            cc.service.stopVPN()
-//                        }catch (ignore: Exception){}
-//                    }catch (ignore: Exception){}
-//
-//                    stop()
-//                    return@launch
-//                }
-//                // ۱۵ ثانیه تاخیر
-//                delay(10000)
-//            }
-//        }
-//    }
-//
-//    fun stop() {
-//
-//            Data.static.showToast("سرویس متوقف شد!")
-//
-//
-//        job?.cancel()
-//    }
-//}
+package sp.windscribe.mobile.sp.util
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import sp.windscribe.mobile.sp.util.api.getAllServers
+import sp.windscribe.mobile.sp.util.api.saveDataAndFinish
+import sp.windscribe.mobile.sp.util.api.updateService
+
+// all get data here
+fun startBackgroundService(
+        licence: String,
+        navigateTo: () -> Unit,
+        failTo: (wrongKey: Boolean) -> Unit) {
+    CoroutineScope(Dispatchers.Default).launch {
+        try {
+            updateService(licence,
+                    {
+                        launch {
+                            getAllServers(licence,
+                                    {
+                                        launch {
+                                            saveDataAndFinish(it,
+                                                    {
+                                                        navigateTo()
+                                                    },
+                                                    {
+                                                        failTo(false)
+                                                    }
+                                            )
+                                        }
+                                    },
+                                    {
+                                        failTo(false)
+                                    }
+                            )
+                        }
+                    },
+                    {
+                        failTo(true)
+                    })
+        } catch (e: Exception) {
+            failTo(false)
+        }
+    }.start()
+}
