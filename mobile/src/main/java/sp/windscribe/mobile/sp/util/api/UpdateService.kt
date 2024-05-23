38caf0a539d18ae51589f5ec81a926819a4a991d
MehraB832 <mehrabshafae@gmail.com>
2024-05-23 15:27:12 +0330

    use MainExecutor

--- mobile/src/main/java/sp/windscribe/mobile/sp/util/api/UpdateService.kt
+++ mobile/src/main/java/sp/windscribe/mobile/sp/util/api/UpdateService.kt
@@ -1,5 +1,6 @@
 package sp.windscribe.mobile.sp.util.api
 
+import android.util.Log
 import com.apollographql.apollo3.api.Error
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -20,67 +21,79 @@ suspend fun updateService(
 
                     override fun onSuccess(data: GetLoginQuery.Data?) {
                         try{
-//                            Data.static.mainApplication.run {
+                            Data.static.MainApplicationExecuter({
                                 // run ViewModel on application thread
+                                Log.d("MRBB", data?.service?.quotaLeft.toString()) // 50843
+                                Log.d("MRBB", data?.service?.dailyQuotaLeft.toString()) // 50843
                                 Data.static.getmViewModel().saveDataLeft(data?.service?.quotaLeft ?: 0)
                                 Data.static.getmViewModel().saveDataDailyLeft(data?.service?.dailyQuotaLeft ?: 0)
-//                            }
+                                Data.static.showToast("runtime no e")
+                            }, Data.static.mainApplication)
                         }catch (e: Exception){
-                            Data.static.showToast("runtime err")
+                            Data.static.showToast("runtime err: ")
+                            Log.d("MRBB O", e.toString())
+                            failTo()
+                            return
                         }
-                        thread {
-                            Data.serviceStorage
-                                    .putString("user_name", data?.service?.name)
+                        Data.serviceStorage
+                                .putString("user_name", data?.service?.name)
 
-                            Data.serviceStorage
-                                    .encode("key_login", licenceKey) // save key for get runtime data service
+                        Data.serviceStorage
+                                .encode("key_login", licenceKey) // save key for get runtime data service
+                        try{
+//                            thread {
+                                /*
+                                    "timeLimited": false,
+                                    "quotaLimited": true,
+                                    "dailyQuotaLimited": false,
+                                */
+                                Data.serviceStorage.putBoolean(
+                                        "timeLimited_service",
+                                        data?.service?.type?.timeLimited ?: false
+                                )
+                                Data.serviceStorage.putBoolean(
+                                        "quotaLimited_service",
+                                        data?.service?.type?.quotaLimited ?: false
+                                )
+                                Data.serviceStorage.putBoolean(
+                                        "dailyQuotaLimited_service",
+                                        data?.service?.type?.dailyQuotaLimited ?: false
+                                )// end type
 
-                            /*
-                                "timeLimited": false,
-                                "quotaLimited": true,
-                                "dailyQuotaLimited": false,
-                            */
-                            Data.serviceStorage.putBoolean(
-                                    "timeLimited_service",
-                                    data?.service?.type?.timeLimited ?: false
-                            )
-                            Data.serviceStorage.putBoolean(
-                                    "quotaLimited_service",
-                                    data?.service?.type?.quotaLimited ?: false
-                            )
-                            Data.serviceStorage.putBoolean(
-                                    "dailyQuotaLimited_service",
-                                    data?.service?.type?.dailyQuotaLimited ?: false
-                            )// end type
+                                // service info
+                                Data.serviceStorage.putInt(
+                                        "dailyQuota_service", // مقدار حجم کل محدودیت روزانه به مگ
+                                        data?.service?.dailyQuota?.toInt() ?: 0
+                                )
+                                Data.serviceStorage.putInt(
+                                        "quotaSum_service", // مقدار حجم کل سرویس به مگ
+                                        data?.service?.quotaSum ?: 0
+                                )
+                                Data.serviceStorage.putInt(
+                                        "daysLeft_service", // مقدار روز باقی مانده تا اتمام سرویس
+                                        data?.service?.daysLeft ?: 0
+                                )
+                                Data.serviceStorage.putInt(
+                                        "days_service", // مقدار روز سرویس
+                                        data?.service?.days ?: 0
+                                )
 
-                            // service info
-                            Data.serviceStorage.putInt(
-                                    "dailyQuota_service", // مقدار حجم کل محدودیت روزانه به مگ
-                                    data?.service?.dailyQuota?.toInt() ?: 0
-                            )
-                            Data.serviceStorage.putInt(
-                                    "quotaSum_service", // مقدار حجم کل سرویس به مگ
-                                    data?.service?.quotaSum ?: 0
-                            )
-                            Data.serviceStorage.putInt(
-                                    "daysLeft_service", // مقدار روز باقی مانده تا اتمام سرویس
-                                    data?.service?.daysLeft ?: 0
-                            )
-                            Data.serviceStorage.putInt(
-                                    "days_service", // مقدار روز سرویس
-                                    data?.service?.days ?: 0
-                            )
-
-                            // usr, pss (vpn)
-                            Data.serviceStorage.putString(
-                                    "username_ovpn",
-                                    data?.service?.username.toString()
-                            )
-                            Data.serviceStorage.putString(
-                                    "password_ovpn",
-                                    data?.service?.password.toString()
-                            )
-                        }.start()
+                                // usr, pss (vpn)
+                                Data.serviceStorage.putString(
+                                        "username_ovpn",
+                                        data?.service?.username.toString()
+                                )
+                                Data.serviceStorage.putString(
+                                        "password_ovpn",
+                                        data?.service?.password.toString()
+                                )
+//                            }.start()
+                        }catch (e: Exception){
+                            Data.static.showToast("runtime err!")
+                            Log.d("MRBB 6", e.toString())
+                            failTo()
+                            return
+                        }
 
                         finishTo() // get servers
                     }
