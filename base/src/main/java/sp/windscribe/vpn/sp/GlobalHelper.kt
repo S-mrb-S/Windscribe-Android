1692e0528a057d0f4f1a4f8a35b0270892714b77
MehraB832 <mehrabshafae@gmail.com>
2024-05-23 15:26:26 +0330

    add MainExecutor

--- base/src/main/java/sp/windscribe/vpn/sp/GlobalHelper.kt
+++ base/src/main/java/sp/windscribe/vpn/sp/GlobalHelper.kt
@@ -1,16 +1,29 @@
-package sp.windscribe.vpn.sp;
+package sp.windscribe.vpn.sp
 
-import android.app.Application;
-import android.content.xversion.api.my.xapi;
+import android.app.Application
+import android.content.xversion.api.my.xapi
+import androidx.annotation.WorkerThread
+import androidx.core.content.ContextCompat
+import sp.windscribe.vpn.model.MainViewModel
 
-import androidx.annotation.Nullable;
+open class GlobalHelper : xapi() {
+    @JvmField
+    protected var mViewModel: MainViewModel? = null
+    //    protected MmkvManager mmkvStorage;
+    @JvmField
+    protected var mainApplication: Application? = null
+    //    protected boolean isImportantErrorBoolean = false;
 
-import sp.windscribe.vpn.model.MainViewModel;
-
-public class GlobalHelper extends xapi {
-    protected MainViewModel mViewModel;
-//    protected MmkvManager mmkvStorage;
-    @Nullable
-    protected Application mainApplication;
-//    protected boolean isImportantErrorBoolean = false;
+    @WorkerThread
+    fun MainApplicationExecuter(run: Runnable, application: Application) {
+        ContextCompat.getMainExecutor(application).execute {
+            run.run()
+        }
+    }
+    @WorkerThread
+    fun MainApplicationExecuter(run: () -> Unit, application: Application) {
+        ContextCompat.getMainExecutor(application).execute {
+            run()
+        }
+    }
 }
