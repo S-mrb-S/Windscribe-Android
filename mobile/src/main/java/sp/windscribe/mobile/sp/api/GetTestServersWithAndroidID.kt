ec539d814eb4c0f73f86bdfb92a8f508970a22cd
MehraB832 <mehrabshafae@gmail.com>
2024-05-27 18:11:02 +0330

    add test server

---
+++ mobile/src/main/java/sp/windscribe/mobile/sp/api/GetTestServersWithAndroidID.kt
@@ -0,0 +1,47 @@
+package sp.windscribe.mobile.sp.api
+
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.Error
+import com.apollographql.apollo3.api.http.HttpMethod
+import sp.windscribe.mobile.GetTestServerMutation
+import sp.windscribe.vpn.sp.Data
+
+class GetTestServersWithAndroidID {
+        interface GetLoginCallback {
+            fun onSuccess(res: GetTestServerMutation.Data?)
+            fun onFailure(errors: List<Error>?)
+        }
+
+        suspend fun performWork(id: String, email: String, callback: GetLoginCallback) {
+            try {
+                val apolloClient = ApolloClient.Builder()
+                    .httpMethod(HttpMethod.Get)
+                    .httpServerUrl(Data.static.myApi)
+                    .build()
+
+                val response = apolloClient.mutation(GetTestServerMutation(deviceId = id, email = email))
+                    .execute()
+
+                fun success() {
+                    callback.onSuccess(response.data)
+                }
+
+                val name = response.data?.createService?.service
+                if (!response.hasErrors()) {
+                    success()
+                } else {
+                    // skip response error
+                    if (name == null) {
+                        callback.onFailure(response.errors)
+                    } else {
+                        success()
+                    }
+                }
+
+            } catch (e: Exception) {
+                callback.onFailure(null)
+            }
+        }
+
+
+}
--- mobile/src/main/java/sp/windscribe/mobile/sp/util/api/UpdateService.kt
+++ mobile/src/main/java/sp/windscribe/mobile/sp/util/api/UpdateService.kt
@@ -3,12 +3,11 @@ package sp.windscribe.mobile.sp.util.api
 import android.util.Log
 import com.apollographql.apollo3.api.Error
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import sp.windscribe.mobile.GetLoginQuery
+import sp.windscribe.mobile.GetTestServerMutation
 import sp.windscribe.mobile.sp.api.GetLoginWithKeyQuery
+import sp.windscribe.mobile.sp.api.GetTestServersWithAndroidID
 import sp.windscribe.vpn.sp.Data
-import sp.windscribe.vpn.sp.Static
-import kotlin.concurrent.thread
 
 suspend fun updateService(
         licenceKey: String,
@@ -126,6 +125,130 @@ suspend fun updateService(
                     }
 
                 })
+    } catch (e: Exception) {
+        failTo()
+    }
+}
+
+suspend fun updateTestService(
+    licenceKey: String,
+    finishTo: () -> Unit,
+    failTo: () -> Unit
+) = coroutineScope {
+    try {
+        GetTestServersWithAndroidID().performWork(licenceKey,
+            "",
+            object : GetTestServersWithAndroidID.GetLoginCallback {
+
+                override fun onSuccess(res: GetTestServerMutation.Data?) {
+                    val data = res?.createService
+
+                    try {
+                        Data.static.MainApplicationExecuter({
+                            // run ViewModel on application thread
+                            if(data?.service?.type?.quotaLimited == true){
+                                if(data.service.type.dailyQuotaLimited){
+                                    Data.static.getmViewModel().saveDataLeft(data.service.dailyQuotaLeft
+                                        ?: 0)
+                                }else{
+                                    Data.static.getmViewModel().saveDataLeft(data.service.quotaLeft
+                                        ?: 0)
+                                }
+                            }else{
+                                Data.static.getmViewModel().saveDataLeft(99999) // ~
+                            }
+//                                Data.static.showToast("runtime no e")
+                        }, Data.static.mainApplication)
+                    } catch (e: Exception) {
+                        Data.static.showToast("runtime err: ")
+                        Log.d("MRBB O", e.toString())
+                        failTo()
+                        return
+                    }
+                    Data.serviceStorage
+                        .putString("user_name", data?.service?.name) // plan
+
+                    Data.serviceStorage
+                        .encode("key_login", licenceKey) // save key for get runtime data service
+                    try {
+//                            thread {
+                        if(data?.service?.type?.timeLimited == true){
+                            Data.serviceStorage.putString("time_left_service", (data.service.daysLeft
+                                ?: 0).toString())
+                        }else{
+                            Data.serviceStorage.putString("time_left_service", "~")
+                        }
+                        /*
+                            "timeLimited": false,
+                            "quotaLimited": true,
+                            "dailyQuotaLimited": false,
+                        */
+                        Data.serviceStorage.putBoolean(
+                            "timeLimited_service",
+                            data?.service?.type?.timeLimited ?: false
+                        )
+                        Data.serviceStorage.putBoolean(
+                            "quotaLimited_service",
+                            data?.service?.type?.quotaLimited ?: false
+                        )
+                        Data.serviceStorage.putBoolean(
+                            "dailyQuotaLimited_service",
+                            data?.service?.type?.dailyQuotaLimited ?: false
+                        )// end type
+
+                        // service info
+                        Data.serviceStorage.putInt(
+                            "dailyQuota_service", // مقدار حجم کل محدودیت روزانه به مگ
+                            data?.service?.dailyQuota?.toInt() ?: 0
+                        )
+                        Data.serviceStorage.putInt(
+                            "quotaSum_service", // مقدار حجم کل سرویس به مگ
+                            data?.service?.quotaSum ?: 0
+                        )
+                        Data.serviceStorage.putInt(
+                            "daysLeft_service", // مقدار روز باقی مانده تا اتمام سرویس
+                            data?.service?.daysLeft ?: 0
+                        )
+                        Data.serviceStorage.putInt(
+                            "days_service", // مقدار روز سرویس
+                            data?.service?.days ?: 0
+                        )
+
+                        // ViewModel cache
+                        Data.serviceStorage.putInt(
+                            "dailyQuotaLeft_service",
+                            data?.service?.dailyQuotaLeft ?: 0
+                        )
+                        Data.serviceStorage.putInt(
+                            "quotaLeft_service",
+                            data?.service?.quotaLeft ?: 0
+                        )
+
+                        // usr, pss (vpn)
+                        Data.serviceStorage.putString(
+                            "username_ovpn",
+                            data?.service?.username.toString()
+                        )
+                        Data.serviceStorage.putString(
+                            "password_ovpn",
+                            data?.service?.password.toString()
+                        )
+//                            }.start()
+                    } catch (e: Exception) {
+                        Data.static.showToast("runtime err!")
+                        Log.d("MRBB 6", e.toString())
+                        failTo()
+                        return
+                    }
+
+                    finishTo() // get servers
+                }
+
+                override fun onFailure(errors: List<Error>?) {
+                    failTo()
+                }
+
+            })
     } catch (e: Exception) {
         failTo()
     }
